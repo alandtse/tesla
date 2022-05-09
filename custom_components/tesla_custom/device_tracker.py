9c7c63db227a2d45f298e44ff25d61a25345e4d5
Michael Benz <michael@benz.io>
2022-05-09 08:18:29 +0000

    Add Device_tracker

--- custom_components/tesla_custom/device_tracker.py
+++ custom_components/tesla_custom/device_tracker.py
@@ -1,46 +1,40 @@
 """Support for tracking Tesla cars."""
-from __future__ import annotations
+import logging
 
-from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
+from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
+from homeassistant.helpers.entity import EntityCategory
 
-from . import DOMAIN as TESLA_DOMAIN
-from .tesla_device import TeslaDevice
+
+from homeassistant.core import HomeAssistant
+
+from . import TeslaDataUpdateCoordinator
+from .base import TeslaBaseEntity
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the Tesla binary_sensors by config_entry."""
-    entities = [
-        TeslaDeviceEntity(
-            device,
-            hass.data[TESLA_DOMAIN][config_entry.entry_id]["coordinator"],
-        )
-        for device in hass.data[TESLA_DOMAIN][config_entry.entry_id]["devices"][
-            "devices_tracker"
-        ]
-    ]
+async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):
+    """Set up the Tesla selects by config_entry."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+
+    entities = []
+    for car in hass.data[DOMAIN][config_entry.entry_id]["cars"]:
+        entities.append(CarLocation(hass, car, coordinator))
+
     async_add_entities(entities, True)
 
 
-class TeslaDeviceEntity(TeslaDevice, TrackerEntity):
-    """A class representing a Tesla device."""
+class CarLocation(TeslaBaseEntity, TrackerEntity):
+    """Representation of the Tesla Car Location Tracker."""
 
-    @property
-    def force_update(self):
-        """Disable forced updated since we are polling via the coordinator updates."""
-        return False
-
-    @property
-    def latitude(self) -> float | None:
-        """Return latitude value of the device."""
-        location = self.tesla_device.get_location()
-        return self.tesla_device.get_location().get("latitude") if location else None
-
-    @property
-    def longitude(self) -> float | None:
-        """Return longitude value of the device."""
-        location = self.tesla_device.get_location()
-        return self.tesla_device.get_location().get("longitude") if location else None
+    def __init__(
+        self, hass: HomeAssistant, car: dict, coordinator: TeslaDataUpdateCoordinator
+    ) -> None:
+        """Initialize the Sensor Entity."""
+        super().__init__(hass, car, coordinator)
+        self.type = "location tracker"
 
     @property
     def source_type(self):
@@ -48,16 +42,40 @@ class TeslaDeviceEntity(TeslaDevice, TrackerEntity):
         return SOURCE_TYPE_GPS
 
     @property
+    def longitude(self) -> float | None:
+        """Return longitude value of the device."""
+        data: dict = self.car.drive
+        if data.get("native_location_supported"):
+            return data.get("native_longitude")
+
+        return data.get("longitude")
+
+    @property
+    def latitude(self) -> float | None:
+        """Return latitude value of the device."""
+        data: dict = self.car.drive
+        if data.get("native_location_supported"):
+            return data.get("native_latitude")
+
+        return data.get("latitude")
+
+    @property
     def extra_state_attributes(self):
         """Return the state attributes of the device."""
-        attr = super().extra_state_attributes.copy()
-        location = self.tesla_device.get_location()
-        if location:
-            attr.update(
-                {
-                    "trackr_id": self.unique_id,
-                    "heading": location["heading"],
-                    "speed": location["speed"],
-                }
-            )
+        data: dict = self.car.drive
+        if data.get("native_location_supported"):
+            heading = data.get("native_heading")
+        else:
+            heading = data.get("heading")
+
+        attr = {
+            "heading": heading,
+            "speed": data.get("speed", 0),
+        }
+
         return attr
+
+    @property
+    def force_update(self):
+        """Disable forced updated since we are polling via the coordinator updates."""
+        return False
