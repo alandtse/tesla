a808d1730a34431fb0423af5bef842cc2a7cd463
shred86 <shred86@icloud.com>
2022-09-06 01:39:41 +0000

    Fix for energy added error

--- custom_components/tesla_custom/sensor.py
+++ custom_components/tesla_custom/sensor.py
@@ -187,7 +187,7 @@ class TeslaCarChargerEnergy(TeslaCarEntity, SensorEntity):
         super().__init__(hass, car, coordinator)
         self.type = "energy added"
         self._attr_device_class = SensorDeviceClass.ENERGY
-        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+        self._attr_state_class = SensorStateClass.TOTAL
         self._attr_native_unit_of_measurement = ENERGY_KILO_WATT_HOUR
         self._attr_icon = "mdi:lightning-bolt"
 
