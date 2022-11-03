c3b0a5832027e1176fff7e802212f6748569587d
MillerKyle72 <millerkyle72@gmail.com>
2022-11-03 15:47:03 -0400

    drop s from window entity name

--- tests/test_cover.py
+++ tests/test_cover.py
@@ -88,7 +88,7 @@ async def test_windows(hass: HomeAssistant) -> None:
         assert await hass.services.async_call(
             COVER_DOMAIN,
             SERVICE_OPEN_COVER,
-            {ATTR_ENTITY_ID: "cover.my_model_s_windows"},
+            {ATTR_ENTITY_ID: "cover.my_model_s_window"},
             blocking=True,
         )
         mock_open_cover.assert_awaited_once()
