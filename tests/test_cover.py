a51fe4be1525841f24eb692e7857ad552f402a90
InTheDaylight14 <67400055+InTheDaylight14@users.noreply.github.com>
2022-11-08 10:46:01 -0500

    missing s from entity check

--- tests/test_cover.py
+++ tests/test_cover.py
@@ -28,8 +28,8 @@ async def test_registry_entries(hass: HomeAssistant) -> None:
     entry = entity_registry.async_get("cover.my_model_s_trunk")
     assert entry.unique_id == f"{car_mock_data.VIN.lower()}_trunk"
 
-    entry = entity_registry.async_get("cover.my_model_s_window")
-    assert entry.unique_id == f"{car_mock_data.VIN.lower()}_window"
+    entry = entity_registry.async_get("cover.my_model_s_windows")
+    assert entry.unique_id == f"{car_mock_data.VIN.lower()}_windows"
 
 
 async def test_charger_door(hass: HomeAssistant) -> None:
