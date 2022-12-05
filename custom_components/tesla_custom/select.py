e81b7e95cbb7bb9b2076f1c9cb445db912997099
MillerKyle72 <millerkyle72@gmail.com>
2022-12-05 09:59:48 -0500

    remoce duplicate section

--- custom_components/tesla_custom/select.py
+++ custom_components/tesla_custom/select.py
@@ -128,11 +128,6 @@ class TeslaCarHeatedSeat(TeslaCarEntity, SelectEntity):
 
             level: int = HEATER_OPTIONS.index(option)
 
-            _LOGGER.debug("Setting %s to %s", self.name, level)
-            await self._car.remote_seat_heater_request(
-                level, SEAT_ID_MAP[self._seat_name]
-            )
-
             if not self._car.is_climate_on and level > 0:
                 await self._car.set_hvac_mode("on")
 
