ab02438e76fa3c5c9d60ad63cb578bec930be626
semantic-release <semantic-release>
2025-07-28 11:55:49 -0700

    3.25.2
    Automatically generated by python-semantic-release

--- custom_components/tesla_custom/const.py
+++ custom_components/tesla_custom/const.py
@@ -1,6 +1,6 @@
 """Const file for Tesla cars."""
 
-VERSION = "3.25.1"
+VERSION = "3.25.2"
 CONF_EXPIRATION = "expiration"
 CONF_INCLUDE_VEHICLES = "include_vehicles"
 CONF_INCLUDE_ENERGYSITES = "include_energysites"
