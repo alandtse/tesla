57281264b90f0b7ddc4770a497e6b36b17c2c643
Fred Wu <ifredwu@gmail.com>
2026-06-13 23:45:13 +1000

    fix: enforce SSL context configuration for TLS 1.3

--- custom_components/tesla_custom/util.py
+++ custom_components/tesla_custom/util.py
@@ -1,5 +1,7 @@
 """Utilities for tesla."""
 
+import ssl
+
 try:
     # Home Assistant 2023.4.x+
     from homeassistant.util.ssl import get_default_context
@@ -9,3 +11,7 @@ except ImportError:
     from homeassistant.util.ssl import client_context
 
     SSL_CONTEXT = client_context()
+
+
+SSL_CONTEXT.minimum_version = ssl.TLSVersion.TLSv1_3
+SSL_CONTEXT.maximum_version = ssl.TLSVersion.TLSv1_3
