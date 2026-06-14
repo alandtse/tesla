d47ff6898ee9247495858a24cc577affbc9ac7c4
MacTh3Mac <34489434+MacTh3Mac@users.noreply.github.com>
2026-06-14 20:08:31 +0100

    fix: cap the Tesla httpx client to TLS 1.2 (#1202)
    closes #1198

--- custom_components/tesla_custom/util.py
+++ custom_components/tesla_custom/util.py
@@ -1,5 +1,9 @@
 """Utilities for tesla."""
 
+import ssl
+
+import httpx
+
 try:
     # Home Assistant 2023.4.x+
     from homeassistant.util.ssl import get_default_context
@@ -9,3 +13,7 @@ except ImportError:
     from homeassistant.util.ssl import client_context
 
     SSL_CONTEXT = client_context()
+
+
+TESLA_SSL_CONTEXT = httpx.create_ssl_context()
+TESLA_SSL_CONTEXT.maximum_version = ssl.TLSVersion.TLSv1_2
